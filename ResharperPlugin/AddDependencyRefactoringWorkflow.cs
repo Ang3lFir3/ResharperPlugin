da33b8845a96524c0143631eccc9e18e9ff72b3d
Kelly <kelly.leahy@milliman.com>
2011-01-16 20:46:20 -0800

    some more work on 'add dependency' refactoring.

--- ResharperPlugin/AddDependencyRefactoringWorkflow.cs
+++ ResharperPlugin/AddDependencyRefactoringWorkflow.cs
@@ -1,10 +1,24 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using JetBrains.ActionManagement;
 using JetBrains.Application.Progress;
+using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CodeStyle;
 using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.CSharp.Util;
+using JetBrains.ReSharper.Psi.ExtensionsAPI.Tree;
+using JetBrains.ReSharper.Psi.Naming;
+using JetBrains.ReSharper.Psi.Naming.Extentions;
+using JetBrains.ReSharper.Psi.Naming.Settings;
+using JetBrains.ReSharper.Psi.Resolve;
+using JetBrains.ReSharper.Psi.Search;
+using JetBrains.ReSharper.Psi.Tree;
 using JetBrains.ReSharper.Refactorings.ChangeSignature;
 using JetBrains.ReSharper.Refactorings.Conflicts;
+using JetBrains.ReSharper.Refactorings.CSharp.ChangeSignature;
 using JetBrains.ReSharper.Refactorings.Workflow;
 using JetBrains.TextControl;
 using DataConstants = JetBrains.ReSharper.Psi.Services.DataConstants;
@@ -16,17 +30,113 @@ namespace ResharperPlugin
         private AddDependencyModel _model;
         private AddDependencyPage _page;
         private IDeclaredElementPointer<ITypeElement> _class;
+        private IDeclaredElementPointer<IConstructor> _ctor;
+        private ISolution _solution;
 
         public bool Execute(IProgressIndicator progressIndicator)
         {
+            var ctor = _ctor.FindDeclaredElement();
+            if (ctor == null)
+                return false;
+            var definingClass = _class.FindDeclaredElement();
+            if (definingClass == null)
+                return false;
+            var factory = CSharpElementFactory.GetInstance(ctor.Module);
+            var ctorDecl = ctor.GetDeclarations().FirstOrDefault();
+            if (ctorDecl == null)
+            {
+                var typeDecl = definingClass.GetDeclarations().First() as IClassLikeDeclarationNode;
+                var typeBody = typeDecl.Body;
+                ctorDecl = factory.CreateTypeMemberDeclaration("public $0() {}", typeDecl.DeclaredName);
+                ctorDecl = ModificationUtil.AddChildBefore(typeBody, typeBody.FirstChild.NextSibling, ctorDecl.ToTreeNode()).GetContainingElement<IConstructorDeclaration>(true);
+            }
+            var type = CSharpTypeFactory.CreateType(_model.NewParameterType, ctorDecl.ToTreeNode());
+            if (!type.IsResolved)
+            {
+                var interfaceDecl = factory.CreateTypeMemberDeclaration("public interface IFoo {}");
+                interfaceDecl.SetName(type.GetPresentableName(CSharpLanguageService.CSHARP));
+                interfaceDecl.LanguageService.CodeFormatter.Format(interfaceDecl.ToTreeNode(), CodeFormatProfile.GENERATOR);
+                var containingTypeDecl = ctor.GetContainingType().GetDeclarations().First();
+                ModificationUtil.AddChildBefore(containingTypeDecl.ToTreeNode(), interfaceDecl.ToTreeNode());
+            }
+            type = CSharpTypeFactory.CreateType(_model.NewParameterType, ctorDecl.ToTreeNode());
+            var naming = PsiManager.GetInstance(_solution).Naming;
+            var suggestionOptions = new SuggestionOptions();
+            var recommendedName = naming.Suggestion.GetDerivedName(
+                type.GetPresentableName(CSharpLanguageService.CSHARP), 
+                NamedElementKinds.Parameters, ScopeKind.Common,
+                CSharpLanguageService.CSHARP, suggestionOptions);
+
+            var parametersOwner = ctorDecl as ICSharpParametersOwnerDeclaration;
+            var references = FindReferences(parametersOwner, progressIndicator);
+
+            parametersOwner.AddParameterDeclarationAfter(
+                ParameterKind.VALUE, type, recommendedName,
+                parametersOwner.ParameterDeclarations.LastOrDefault());
+
+            foreach (var reference in references)
+                ChangeReference(reference, recommendedName, type);
+
             return true;
+        }
+
+        private void ChangeReference(IArgumentsOwner reference, string recommendedName, IType type)
+        {
+            var csharpOwner = reference as ICSharpArgumentsOwner;
+            var factory = CSharpElementFactory.GetInstance(type.Module);
+            var inField = false;
+            if (csharpOwner.GetContainingElement<IFieldDeclaration>(false) != null)
+                inField = true;
+            var expression = factory.CreateExpression(inField ? "TODO" : recommendedName);
+            csharpOwner.AddArgumentAfter(
+                factory.CreateArgument(ParameterKind.VALUE, null, expression),
+                csharpOwner.Arguments.LastOrDefault());
+        }
+
+        private List<IArgumentsOwner> FindReferences(ICSharpParametersOwnerDeclaration parametersOwner, IProgressIndicator progressIndicator)
+        {
+            var references = new List<IArgumentsOwner>();
+            var consumer = new FindResultConsumer(
+                r =>
+                {
+                    var owners = new HashSet<IArgumentsOwner>();
+                    var reference = r as FindResultReference;
+                    if(reference != null)
+                    {
+                        var ref2 = reference.Reference;
+                        var resolveType = ref2.CheckResolveResult();
+                        if (resolveType == ResolveErrorType.INCORRECT_PARAMETER_NUMBER)
+                            return FindExecution.Continue;
+                        var argumentsOwner = ref2.GetElement().GetContainingElement<IArgumentsOwner>(true);
+                        if(argumentsOwner != null)
+                        {
+                            if (owners.Contains(argumentsOwner))
+                                return FindExecution.Continue;
+                            owners.Add(argumentsOwner);
+                            references.Add(argumentsOwner);
+                        }
+                    }
+                    return FindExecution.Continue;
+                });
+            var searchAction = new SearchAction(
+                parametersOwner.GetManager().Finder,
+                parametersOwner.DeclaredElement,
+                consumer,
+                SearchPattern.FIND_USAGES);
+            searchAction.Task(progressIndicator);
+            return references;
         }
 
         public bool Initialize(IDataContext context)
         {
+            _solution = context.GetData(JetBrains.IDE.DataConstants.SOLUTION);
             _model = new AddDependencyModel();
             _page = new AddDependencyPage(_model, context.GetData(JetBrains.IDE.DataConstants.SOLUTION));
-            _class = GetClass(context).CreateElementPointer();
+            var @class = GetClass(context);
+            _class = @class.CreateElementPointer();
+            var ctor = @class.Constructors.FirstOrDefault();
+            if (ctor != null)
+                _ctor = ctor.CreateElementPointer();
             return true;
         }
 
